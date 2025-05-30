e3919734d7713ffab29113b636dd187a27a4caa7
Telman13 <teliyev10@std.beu.edu.az>
2025-05-30 18:14:40 +0400

    Project architecture initialized and entities created

---
+++ Entities/Entities/Message.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Common;
+using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Message : BaseEntity
+    {
+        public string Description { get; set; }
+        public MessageStatus Status { get; set; } = MessageStatus.Pending;
+        public Guid SenderId { get; set; }
+        public User Sender { get; set; }
+        public Guid ReceiverId { get; set; }
+        public User Receiver { get; set; }
+        public Guid OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
---
+++ Entities/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class User : AppUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+        public ICollection<Message> SentMessages { get; set; }
+        public ICollection<Message> ReceivedMessages { get; set; }
+    }
+}
