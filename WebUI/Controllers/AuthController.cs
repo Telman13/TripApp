1df7c75daafa1233db8ce299f1b49f4e6f6c0c8b
Telman13 <teliyev10@std.beu.edu.az>
2025-12-26 19:40:17 +0400

    feat: Added login page

--- WebUI/Controllers/AuthController.cs
+++ WebUI/Controllers/AuthController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Entities.Dtos.AuthDtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Controllers
 {
-    public class AuthController : Controller
+    public class AuthController(IAuthService auth) : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+
+            var result = await auth.LoginAsync(dto);
+
+            if (result.Success)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ErrorMessageFromLogin = "Invalid username or password.";
+            return View(dto);
         }
     }
 }
